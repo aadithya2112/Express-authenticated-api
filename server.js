7238b6a69f8777ab7343cdbb8cd2b669749cf3d6
aadithya <aadithyacps@gmail.com>
2024-09-08 18:35:12 +0530

    use cors

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+const cors = require("cors");
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // MongoDB connection
 mongoose.connect(process.env.MONGODB_URI)
